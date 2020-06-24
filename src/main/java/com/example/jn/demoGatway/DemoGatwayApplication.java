f29fd9a272b93e70c358ee8f0d0bf631de3794a9
Xiaozhu Yang <xzyang@thoughtworks.com>
2020-06-24 16:49:12 +0800

    user Eureka application name in gateway and it works

--- src/main/java/com/example/jn/demoGatway/DemoGatwayApplication.java
+++ src/main/java/com/example/jn/demoGatway/DemoGatwayApplication.java
@@ -2,26 +2,9 @@ package com.example.jn.demoGatway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoGatwayApplication {
-
-//	@Bean
-//	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//		return builder.routes()
-//				.route(r -> r.path("/emails/**")
-//						.uri("http://localhost:8081/")
-//						.id("email-service"))
-//
-//				.route(r -> r.path("/users/**")
-//						.uri("http://localhost:8080/")
-//						.id("user-service"))
-//				.build();
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGatwayApplication.class, args);
